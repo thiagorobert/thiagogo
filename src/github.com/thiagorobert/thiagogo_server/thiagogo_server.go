5804caf33c9494079878c38cd4a53ffa1faff654
Thiago Santos <thiagorobert@gmail.com>
2015-07-08 18:23:10 -0700

    Add a simple handler that pushes an entry to a local Redis DB.

--- src/github.com/thiagorobert/thiagogo_server/thiagogo_server.go
+++ src/github.com/thiagorobert/thiagogo_server/thiagogo_server.go
@@ -6,19 +6,39 @@ import (
     "net/http"
     "strconv"
     "github.com/thiagorobert/thiagogo_util"
-//    "github.com/garyburd/redigo/redis"
+    "github.com/garyburd/redigo/redis"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello world!")
+    io.WriteString(w, "<HTML><H1><FONT COLOR='red'>Jennifer, I love you!!!!i</FONT></H1></HTML>")
+}
+
+func add_timestamp(w http.ResponseWriter, r *http.Request) {
+    timestamp := "test"
+    c, err := redis.Dial("tcp", ":6379")
+    if err != nil {
+        panic(err)
+    }
+    defer c.Close()
+
+    //set
+    c.Do("SET", timestamp, timestamp)
+
+    //get
+    word, err := redis.String(c.Do("GET", timestamp))
+    if err != nil {
+        fmt.Println("key not found")
+    }
+
+    io.WriteString(w, "<HTML><H1><FONT COLOR='red'>" + word + "</FONT></H1></HTML>")
 }
 
 func main() {
-//    n, err := conn.Do("APPEND", "key", "value")
     port := 8042
     fmt.Printf("starting server at port " + strconv.Itoa(port) + "\n")
     fmt.Printf(thiagogo_util.Reverse("Thiago Robert"))
     http.HandleFunc("/", hello)
-    // http.ListenAndServe(":" + strconv.Itoa(port), nil)
+    http.HandleFunc("/add", add_timestamp)
+    http.ListenAndServe(":" + strconv.Itoa(port), nil)
 }
 
